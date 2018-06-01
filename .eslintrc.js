1bff78ed9d5db3cfe827455e4f003a72bdca81c8
Eka Risyana <risyana@gmail.com>
2018-06-01 09:39:58 +0700

    add eslint as dev depencency

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    "extends": "airbnb",
+    "globals": {
+        "document": true,
+        "window": true,
+        "navigator": true,
+        "fetch": true
+    },
+    "rules": {
+        "linebreak-style": ["error", "windows"],
+        "react/jsx-filename-extension": false
+    }
+};
