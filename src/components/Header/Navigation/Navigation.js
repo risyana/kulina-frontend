43269235129eaa24e7d5d71e2278798918b8cfc1
Eka Risyana <risyana@gmail.com>
2018-06-17 13:41:00 +0700

    create sticky navitation bar when scrolling-top reach 100px

--- src/components/Header/Navigation/Navigation.js
+++ src/components/Header/Navigation/Navigation.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import NavigationItems from './NavigationItems/NavigationItems';
+import BurgerIcon from '../BurgerIcon/BurgerIcon';
 
 const Navigation = props => (
   <nav className="NavDesktop">
     <NavigationItems />
+    <BurgerIcon onClickBackdrop={props.onClickBackdrop} />
   </nav>
 );
 
